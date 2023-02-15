cd8ed3ed093c9d7b926e40943ceb2b3bbb38ff9c
大大怪 <123348658+suitsunshine@users.noreply.github.com>
2023-02-15 12:24:37 +0800

    整理01

--- demo01.cpp
+++ demo01.cpp
@@ -1,21 +1,12 @@
 //坤坤大战
 #include<iostream>
 #include<graphics.h>
-
-#define high 890
-#define width 480
 //加载玩家图片
 IMAGE ima_kunkun[2];// 玩家
 IMAGE bk;
 
-void load_imag() {
-	loadimage(&bk, "resource/back01.png", 501, 999, true); //放在同级目录下才可以加载
-	//加载玩家图片
-	loadimage(ima_kunkun + 0,"resource/R-C.jpg",50,50);
-	loadimage(ima_kunkun + 1,"resource/R-C (2).jpg",50,50);
-}
-
-
+#define high 890
+#define width 480
 //坤哥的属性
 struct player {
 	int x;
@@ -24,28 +15,38 @@ struct player {
 };
 struct player k;
 
-void kunkun_init(player* pthis, int x, int y) {
+void kunkun_init(player* pthis, int x, int y) { //属性初始化
 	pthis->x = x;
 	pthis->y = y;
 	pthis->isDie = false;
 }
 
 //所有的数据初始化
+
+void load_imag() {
+	loadimage(&bk, "resource/back01.png", 501, 999, true); //放在同级目录下才可以加载
+	loadimage(ima_kunkun + 0, "resource/R-C.jpg", 50, 50);
+	loadimage(ima_kunkun + 1, "resource/R-C (2).jpg", 50, 50);
+}
 void init() {
+	load_imag();
+	kunkun_init(&k, (getwidth() - ima_kunkun->getwidth()) / 2, getheight() - high);
 	kunkun_init(&k, 250, 892);
+
 }
 
 //绘制界面
 void draw() {
-	putimage(0, 0, &bk);
-	putimage(0, 0, ima_kunkun + 0);
-	putimage(0, 0, ima_kunkun + 1);
+	putimage(0, -10, &bk);
+	putimage(k.x, k.y, ima_kunkun + 0);
+	putimage(k.x, k.y, ima_kunkun + 1);
 }
+
+
 int main() {
 	initgraph(500,892, EX_SHOWCONSOLE);
-	init();
-	//贴背景图
 	load_imag();
+	init();
 	draw();
 	system("pause");
 	return 0;
